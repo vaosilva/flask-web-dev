fe2a8c88e8f5da892d1db2c7c87fa71878fc354f
vaosilva <svitor95@hotmail.com>
2020-09-24 23:45:49 +0100

    Python virtual environments commands for Powershell

---
+++ tips_virtual_environments.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+# criar virtual environment:
+python -m venv venv 
+# activar:
+Set-ExecutionPolicy Unrestricted -Scope Process
+venv\Scripts\activate
+# desactivar:
+deactivate
